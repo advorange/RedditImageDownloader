d1733d2fbb5047d9b0610a9077f29333d707bf31
advorange <advorange@live.com>
2017-12-18 20:10:53 -0800

    Renamed the base directory.

--- src/ImageDL/ImageDownloaders/IImageDownloader.cs
+++ src/ImageDL/ImageDownloaders/IImageDownloader.cs
@@ -2,12 +2,30 @@
 
 namespace ImageDL.ImageDownloaders
 {
+	/// <summary>
+	/// Abstraction of <see cref="ImageDownloader{TPost}"/>.
+	/// </summary>
 	public interface IImageDownloader
 	{
 		bool IsReady { get; }
+		string Directory { get; set; }
+		int AmountToDownload { get; set; }
+		int MinWidth { get; set; }
+		int MinHeight { get; set; }
 
+		/// <summary>
+		/// Start downloading images.
+		/// </summary>
+		/// <returns>An asynchronous task which downloads images.</returns>
 		Task StartAsync();
+		/// <summary>
+		/// Sets arguments with only text/user input.
+		/// </summary>
+		/// <param name="args">The text to set.</param>
 		void SetArguments(string[] args);
+		/// <summary>
+		/// Prints to the console what arguments are still needed.
+		/// </summary>
 		void AskForArguments();
 	}
 }
